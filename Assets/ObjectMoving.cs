e66c90e664c2700204df4f2f18d6da534546f200
mike <milkpogs@gmail.com>
2026-04-28 21:23:43 +0200

    fixed movement + added fish sprites
    made plater attach to moving platforms

--- Assets/ObjectMoving.cs
+++ Assets/ObjectMoving.cs
@@ -1,10 +1,13 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class moving : MonoBehaviour
+public class ObjectMoving : MonoBehaviour
 {
     public float min = 2f;
     public float max = 3f;
+    public Transform platform;
+    public Transform player;
     // Use this for initialization
     void Start()
     {
@@ -22,5 +25,18 @@ public class moving : MonoBehaviour
         transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.SetParent(platform);
+       
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.SetParent(null);
+        
+    }
 }
-    
+
