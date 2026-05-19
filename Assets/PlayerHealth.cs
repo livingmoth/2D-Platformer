f3473a0a24877fbdca9c77a8c227000c1760cc80
mike <milkpogs@gmail.com>
2026-05-19 15:27:46 +0200

    changes to lvls 1-3 + spike death

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -48,6 +48,12 @@ public class PlayerHealth : MonoBehaviour
             CanRecieveDamage = false;
             StartCoroutine(InvincibilityTimer(invincibilityTimer, ResetInvincibility));
         }
+        if (Health <= 0)
+        {
+            SceneManager.LoadScene("GameFail");
+        }
+
+
 
 
 
